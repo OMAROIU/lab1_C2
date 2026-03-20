6386e49a528560e34100c899b1f1c69807bbd05a
OMAROIU <omarvasquez1436@gmail.com>
2026-03-20 11:05:44 -0600

    new file:   lab1C2/.gitignore 	new file:   lab1C2/.vscode/extensions.json 	new file:   lab1C2/README.md 	new file:   lab1C2/index.html 	new file:   lab1C2/package-lock.json 	new file:   lab1C2/package.json 	new file:   lab1C2/public/favicon.svg 	new file:   lab1C2/public/icons.svg 	new file:   lab1C2/src/App.vue 	new file:   lab1C2/src/assets/hero.png 	new file:   lab1C2/src/assets/vite.svg 	new file:   lab1C2/src/assets/vue.svg 	new file:   lab1C2/src/components/HelloWorld.vue 	new file:   lab1C2/src/main.js 	new file:   lab1C2/src/style.css 	new file:   lab1C2/vite.config.js

---
+++ lab1C2/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <h1>Lista de Tareas</h1>
+
+    <!-- INPUT -->
+    <input v-model="nuevaTarea" placeholder="Escribe una tarea">
+
+    <!-- BOTON -->
+    <button @click="agregarTarea">Agregar</button>
+
+    <!-- VALIDACION -->
+    <p v-if="error" style="color:red">{{ error }}</p>
+
+    <!-- LISTA -->
+    <ul>
+      <li v-for="(tarea, index) in tareas" :key="index">
+        {{ tarea }}
+        <button @click="eliminar(index)">X</button>
+      </li>
+    </ul>
+
+    <!-- CONTADOR -->
+    <p>Total tareas: {{ tareas.length }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nuevaTarea: "",
+      tareas: [],
+      error: "",
+      usuario: "Omar",
+      activo: true
+    }
+  },
+  methods: {
+    agregarTarea() {
+      if (this.nuevaTarea === "") {
+        this.error = "No puede estar vacío"
+        return
+      }
+
+      this.tareas.push(this.nuevaTarea)
+      this.nuevaTarea = ""
+      this.error = ""
+    },
+    eliminar(index) {
+      this.tareas.splice(index, 1)
+    }
+  }
+}
+</script>
